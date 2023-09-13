f12ab97b64bacfae148d46440218170a3a38381a
SizyLuvsU <123562398+SizyLuvsU@users.noreply.github.com>
2023-09-13 22:04:29 +0300

    Update typed.js

--- typed.js
+++ typed.js
@@ -1,5 +1,5 @@
 new Typed('#typed',{
-  strings : ['Thank God every day that I aint dead','Custom domain','Made by noutaja','Discord: @poliisi.fi','Join the discord server','https://discord.gg/PncmmWJ2tv','Lol'],
+  strings : ['Thank God every day that I aint dead','Custom website','Made by noutaja','Discord: @poliisi.fi','Join the discord server','https://discord.gg/PncmmWJ2tv','Lol'],
   typeSpeed : 35,
   delaySpeed : 90,
   showCursor: false,
