1d0ab988f6fb7f26d11f51b23a40007ddda6f623
SizyLuvsU <123562398+SizyLuvsU@users.noreply.github.com>
2023-09-14 15:57:50 +0300

    Delete audio.js

--- audio.js
+++
@@ -1,7 +0,0 @@
-var audio = new Audio('./audio.mp3');
-audio.play(); //Error (Reason = Google Autoplay Services claims user doesn't interact with page, however that's wrong (False Error))
-function play() {
-var audio = new Audio('./audio.mp3');
-audio.loop = true;
-audio.play();
-}
