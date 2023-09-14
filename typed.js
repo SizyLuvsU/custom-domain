bb44a6eeaaf97704ba0bbde07684531fc85b4d9c
SizyLuvsU <123562398+SizyLuvsU@users.noreply.github.com>
2023-09-14 15:58:34 +0300

    Delete typed.js

--- typed.js
+++
@@ -1,7 +0,0 @@
-new Typed('#typed',{
-  strings : ['Thank God every day that I aint dead','Custom website','Made by noutaja','Discord: @poliisi.fi','Join the discord server','https://discord.gg/PncmmWJ2tv','Lol'],
-  typeSpeed : 35,
-  delaySpeed : 90,
-  showCursor: false,
-  loop : true
-})
